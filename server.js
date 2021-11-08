85c6a301a226ac1df2f9b7b36813305e6ba3ff4c
EH16 <zeroeh16@gmail.com>
2021-11-07 22:50:12 -0600

    carga de cliente 8

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express();
 app.use(express.static('/dist/cliente'));
 app.get('/*', (req,res) =>
-    res.sendFile('inicio.html', {root: 'dist/cliente/src/app/Components/inicio'}),
+    res.sendFile('inicio.component.html', {root: 'dist/cliente/src/app/Components/inicio'}),
 );
 app.listen(process.env.PORT || 8080);
