acba221a630645979673928c92eb291cfbe7d172
EH16 <zeroeh16@gmail.com>
2021-11-07 22:27:37 -0600

    carga de cliente 4

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/cliente'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/cliente/index.html'));});
+app.use(express.static('/dist/cliente'));
+app.get('/*', (req,res) =>
+    res.sendFile('index.html', {root: 'dist/cliente/'}),
+);
 app.listen(process.env.PORT || 8080);
